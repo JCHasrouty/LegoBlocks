04c21d99613e0379f7afb7eff75936e965631971
Jean Claude Hasrouty <jchasrouty@Jeans-MBP.socal.rr.com>
2019-09-19 19:28:01 -0700

    :lipstick: Added Main Menu GUI & Settings

--- LegoBricksDesign/Assets/Scripts/GameMaster.cs
+++ LegoBricksDesign/Assets/Scripts/GameMaster.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,11 +8,11 @@ public class GameMaster : MonoBehaviour
     // reference to the plate 
     public GameObject legoPlateUnitPref;
     // reference to the various lego blocks
-    public GameObject[] legoBlockUniyPref;
+    public GameObject[] legoBlockUnitPref;
     // array to hold various material colors
     public Material[] blockMaterials;
 
-    public float delta_time = 0.5f;
+    public float delta_time = 3.5f;
     float reset_time;
 
     float x, z;
@@ -30,17 +31,28 @@ public class GameMaster : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //main_camera = Camera.main;
         // instantiate a plate of legos
+        if (PlayerPrefs.HasKey("X"))
+        {
+            platform_size = Convert.ToInt32(PlayerPrefs.GetString("X"));
+
+        }
+
         for (int i = 0; i < platform_size; i++)
         {
             for (int j = 0; j < platform_size; j++)
             {
                 // multiply by size of legos to make up for displacement so they hug each other
-                x = i * 0.08f + (startX + 0.04f);
-                z = j * 0.08f + (startZ + 0.04f);
+                // Set to 8f - 4f if using 1 meter bricks
+                // Create legos in maya using meters but set to .008 etc
+                x = (i * 0.08f) + (startX + 0.04f);
+                z = (j * 0.08f) + (startZ + 0.04f);
                 // set location of plate legos and instantiate them
                 var gObj = GameObject.Instantiate(legoPlateUnitPref, new Vector3(x, 0, z), Quaternion.identity);
+                //gObj.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+                gObj.GetComponentInChildren<Rigidbody>().isKinematic = true;
+
+                //Rigidbody gObj1 = gObj.GetComponent<Rigidbody>();
             }
         }
         // dynamically create a game object at run time to be place at the center (0,0,0) by referencing the game object given to it
@@ -55,33 +67,58 @@ public class GameMaster : MonoBehaviour
         if (reset_time < Time.time)
         {
             // randomize and place game objects
-            x = (Random.Range(0, platform_size) * 0.08f) + (startX + 0.04f);
-            z = (Random.Range(0, platform_size) * 0.08f) + (startZ + 0.04f);
-            var gObj = GameObject.Instantiate(legoBlockUniyPref[Random.Range(0, 2)], new Vector3(x, Random.Range(1, 3), z), Quaternion.identity);
+            x = (UnityEngine.Random.Range(0, platform_size) * 0.08f) + (startX + 0.04f);
+            z = (UnityEngine.Random.Range(0, platform_size) * 0.08f) + (startZ + 0.04f);
+            // Spawning game object from UniyPref array, choosing x,y,z coordinates and setting rotation
+            // Edit Random.Range(50,1000) to alter Y coordinate to spawn higher
+            var gObj = GameObject.Instantiate(legoBlockUnitPref[UnityEngine.Random.Range(0, 2)], new Vector3(x, UnityEngine.Random.Range(1, 3), z), Quaternion.identity);
+            // command not working
+            gObj.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+            gObj.GetComponent<Rigidbody>().isKinematic = false;
 
-            Material m = blockMaterials[Random.Range(0, 4)];
+            Material m = blockMaterials[UnityEngine.Random.Range(0, 4)];
             foreach (var r in gObj.GetComponentsInChildren<Renderer>())
                 r.material = m;
 
             //if (physicsOnOff)
-            gObj.GetComponentInChildren<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+            //gObj.GetComponentInChildren<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
 
             // Destroy the objects spawning after a specific amount of time
             Destroy(gObj, delta_time + 15);
             reset_time = Time.time + delta_time;
         }
         //#endregion
+        float x_rot = 0;
+        float y_rot = 0;
+        float z_rot = 0;
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Debug.Log("This is executing now, Key press R");
+            legoBlockUnitPref[0].transform.Rotate(x_rot, y_rot + 90, z_rot);
+        }
+        //RotateLego();
     }
 
-    //void RotateCamera()
-    //{
-    //    if (Input.GetKeyDown(KeyCode.A))
-    //    {
-    //        main_camera.transform.SetPositionAndRotation(Vector3.left, Quaternion.identity);
-    //    }
-    //}
+    /*private void RotateLego()
+    {
+        float x_rot = 0;
+        float y_rot = 0;
+        float z_rot = 0;
+        var gObj = GameObject.Find("_GameManager").name;
+        if (Input.GetKeyDown(KeyCode.R))
+            gObj.transform.Rotate(x_rot + 90, y_rot, z_rot);
+        if (Input.GetKeyDown(KeyCode.T))
+            gObj.transform.Rotate(x_rot, y_rot + 90, z_rot);
+        if (Input.GetKeyDown(KeyCode.Y))
+            gObj.transform.Rotate(x_rot, y_rot, z_rot + 90);
+    }*/
 }
+
+
 
 // If D is pressed turn iskinematics on to essentially break the platform.
 // Kinematics on for the platform and the blocks
 // They will collide and whatever happens happens then, you can toggle it on and off
+
+// Scale guide
+//http://www.gamesmaderight.com/maya-to-unity-scale-guide/
---
+++ LegoBricksDesign/Assets/Scripts/UIControl.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIControl : MonoBehaviour
+{
+    // only way to reference the panel
+    public RectTransform panelMainMenu;
+    public RectTransform panelSetting;
+
+    public InputField inpXDim;
+    public InputField inpYDim;
+
+    public void butStartClicked()
+    {
+        SceneManager.LoadScene("MainLevel");
+        Debug.Log("Button start clicked");
+    }
+    public void butSetupClicked()
+    {
+        // hide main menu display setup
+        panelMainMenu.transform.gameObject.SetActive(false);
+        panelSetting.transform.gameObject.SetActive(true);
+        Debug.Log("Button setup clicked");
+        
+    }
+    public void butExitClicked()
+    {
+        Application.Quit();
+        Debug.Log("Button exit clicked");
+    }
+    public void butSetupOkClicked()
+    {
+        // no error checking but take it into consideration
+        int x = Convert.ToInt32(inpXDim.text);
+        int y = Convert.ToInt32(inpYDim.text);
+
+        PlayerPrefs.SetString("X", x.ToString());
+        PlayerPrefs.SetString("Y", y.ToString());
+
+        Debug.Log(string.Format("The new dimension of board will be {0}x{1}", x, y));
+
+        panelSetting.transform.gameObject.SetActive(false);
+        panelMainMenu.transform.gameObject.SetActive(true);
+    }
+}
